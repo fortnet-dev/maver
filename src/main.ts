846d659b27fd9ca96f3b29c05868e6be4fb4a9df
Oskar Sommer <oskar.sommer@t-systems.com>
2024-08-15 16:06:29 +0200

    wow

--- src/main.ts
+++ src/main.ts
@@ -14,54 +14,23 @@ import { RenderPass } from "three/addons/postprocessing/RenderPass.js"
 const scene = new THREE.Scene()
 const renderer = new THREE.WebGLRenderer({})
 const composer = new EffectComposer(renderer)
-const gui = new GUI()
 
 const CANYON_SCALE = 0.0625
 const parameters = {
 	loopDuration: 120e3,
 	targetOffset: 4e3,
-	glowColor: new THREE.Color(0xbf40bf),
-	canyonColor: new THREE.Color(0x1e49d6),
+	fogColor: new THREE.Color("hsl(291, 100%, 50%)"),
+	debugSplines: true,
 }
-
-const paramGroupGui = gui.addFolder("Parameters")
-paramGroupGui.add(parameters, "loopDuration", 60e3, 240e3).name("Loop Duration")
-paramGroupGui.add(parameters, "targetOffset", 0, 10e3).name("Target Offset")
-
-gui
-	.addColor(parameters, "glowColor")
-	.name("Glow Color")
-	.onChange(() => {
-		scene.background = parameters.glowColor
-		scene.fog?.color.copy(parameters.glowColor)
-	})
-gui
-	.addColor(parameters, "canyonColor")
-	.name("Canyon Color")
-	.onChange(() => {
-		console.log(canyon.scene.getObjectById(0))
-	})
-
-scene.background = parameters.glowColor
-scene.fog = new THREE.Fog(parameters.glowColor, 0.01, 1000)
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
-	10,
+	22,
 	window.innerWidth / window.innerHeight,
 )
 
-gui
-	.add(camera, "fov", 10, 120)
-	.name("Camera FOV")
-	.onChange(() => camera.updateProjectionMatrix())
-
-// Somthin Light
-
-const light = new THREE.PointLight(0xbf40bf, 10e3)
-// scene.add(light)
-
-gui.add(light, "intensity", 0, 100e3).name("Light Intensity")
+// --------------------------------------------------------------------------------
+// Models
 
 // Canyon
 const loader = new GLTFLoader()
@@ -69,6 +38,9 @@ const canyon = await loader.loadAsync("/canyon.glb")
 canyon.scene.scale.set(CANYON_SCALE, CANYON_SCALE, CANYON_SCALE)
 
 scene.add(canyon.scene)
+
+const debugGroup = new THREE.Group()
+scene.add(debugGroup)
 
 // Camera path
 const cameraSpline = await loader.loadAsync("/camera.glb")
@@ -89,14 +61,14 @@ const cameraSplineGeometry = new THREE.BufferGeometry().setFromPoints(
 )
 const cameraSplineMat = new THREE.LineBasicMaterial({ color: 0xff0000 })
 const cameraLine = new THREE.Line(cameraSplineGeometry, cameraSplineMat)
-scene.add(cameraLine)
+debugGroup.add(cameraLine)
 
 const targetSplineGeometry = new THREE.BufferGeometry().setFromPoints(
 	targetVectors,
 )
 const targetSplineMat = new THREE.LineBasicMaterial({ color: 0x00ff00 })
 const targetLine = new THREE.Line(targetSplineGeometry, targetSplineMat)
-scene.add(targetLine)
+debugGroup.add(targetLine)
 
 const targetBall = new THREE.Mesh(
 	new THREE.SphereGeometry(0.5, 32, 32),
@@ -104,35 +76,59 @@ const targetBall = new THREE.Mesh(
 		color: 0x00ff00,
 	}),
 )
-scene.add(targetBall)
+debugGroup.add(targetBall)
 
 //Add CatmullRomCurve3  😍 to follow along path
 const cameraPath = new THREE.CatmullRomCurve3(cameraVectors, true)
 const targetPath = new THREE.CatmullRomCurve3(targetVectors, true)
 
 // --------------------------------------------------------------------------------
+// Postprocessing
 
 composer.addPass(new RenderPass(scene, camera))
 composer.addPass(new OutputPass())
 
 renderer.shadowMap.enabled = true
 
-const resize = () => {
-	camera.aspect = window.innerWidth / window.innerHeight
-	camera.updateProjectionMatrix()
-	renderer.setSize(window.innerWidth, window.innerHeight)
-	composer.setSize(window.innerWidth, window.innerHeight)
-}
-// Resize listener on the window
-resize()
-window.addEventListener("resize", resize)
+scene.background = parameters.fogColor
+scene.fog = new THREE.Fog(parameters.fogColor, 0.01, 1000)
 
-document.body.appendChild(renderer.domElement)
+// --------------------------------------------------------------------------------
+// GUI
+
+const gui = new GUI()
+
+gui
+	.add(camera, "fov", 10, 120)
+	.name("Camera FOV")
+	.onChange(() => camera.updateProjectionMatrix())
+
+const paramGroupGui = gui.addFolder("Parameters")
+paramGroupGui.add(parameters, "loopDuration", 60e3, 240e3).name("Loop Duration")
+paramGroupGui.add(parameters, "targetOffset", 0, 10e3).name("Target Offset")
+
+gui
+	.addColor(parameters, "fogColor")
+	.name("Glow Color")
+	.onChange(() => {
+		scene.background = parameters.fogColor
+		scene.fog?.color.copy(parameters.fogColor)
+	})
+
+gui
+	.add(parameters, "debugSplines")
+	.name("Debug Splines")
+	.onChange(() => {
+		debugGroup.visible = parameters.debugSplines
+	})
+
+// --------------------------------------------------------------------------------
+// Animation
 
 function animate() {
 	stats.begin()
 
-	const time = Date.now()
+	const time = performance.now()
 	const looptimeSeconds = parameters.loopDuration
 	const cameraTime = (time % looptimeSeconds) / looptimeSeconds
 	const targetTime =
@@ -140,7 +136,6 @@ function animate() {
 
 	const cameraPos = cameraPath.getPointAt(cameraTime)
 	camera.position.copy(cameraPos)
-	light.position.copy(cameraPos)
 
 	const targetPos = targetPath.getPointAt(targetTime)
 	targetBall.position.copy(targetPos)
@@ -150,7 +145,20 @@ function animate() {
 	stats.end()
 }
 
+// --------------------------------------------------------------------------------
+// Setup
+
+const resize = () => {
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	composer.setSize(window.innerWidth, window.innerHeight)
+}
+resize()
+window.addEventListener("resize", resize)
+
+document.body.appendChild(renderer.domElement)
+renderer.setAnimationLoop(animate)
+
 const stats = new Stats()
 document.body.appendChild(stats.dom)
-
-renderer.setAnimationLoop(animate)
