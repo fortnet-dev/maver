6b2f0c56e5586cf52b0eef015920863a136020f4
Oskar Sommer <oskar.sommer@t-systems.com>
2024-08-15 19:17:24 +0200

    tabs

--- src/vertexShader.ts
+++ src/vertexShader.ts
@@ -1,31 +1,31 @@
 export const canyonVertexShader = `
-  varying vec3 vWorldPosition;
-  void main() {
-    vWorldPosition = (modelMatrix * vec4(position, 1.0)).xyz;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-  }
+	varying vec3 vWorldPosition;
+	void main() {
+		vWorldPosition = (modelMatrix * vec4(position, 1.0)).xyz;
+		gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+	}
 `
 export const canyonFragmentShader = `
-  uniform vec3 fogColorNear;
+	uniform vec3 fogColorNear;
 	uniform vec3 fogColorMid;
-  uniform vec3 fogColorFar;
-  uniform float fogNear;
-  uniform float fogMid;
-  uniform float fogFar;
-  varying vec3 vWorldPosition;
+	uniform vec3 fogColorFar;
+	uniform float fogNear;
+	uniform float fogMid;
+	uniform float fogFar;
+	varying vec3 vWorldPosition;
 
-  void main() {
-    float depth = distance(vWorldPosition, cameraPosition);
+	void main() {
+		float depth = distance(vWorldPosition, cameraPosition);
 
-    vec3 fogColor;
-    if (depth < fogMid) {
-      float t = smoothstep(fogNear, fogMid, depth);
-      fogColor = mix(fogColorNear, fogColorMid, t);
-    } else {
-      float t = smoothstep(fogMid, fogFar, depth);
-      fogColor = mix(fogColorMid, fogColorFar, t);
-    }
+		vec3 fogColor;
+		if (depth < fogMid) {
+			float t = smoothstep(fogNear, fogMid, depth);
+			fogColor = mix(fogColorNear, fogColorMid, t);
+		} else {
+			float t = smoothstep(fogMid, fogFar, depth);
+			fogColor = mix(fogColorMid, fogColorFar, t);
+		}
 
-    gl_FragColor = vec4(fogColor, 1.0);
-  }
+		gl_FragColor = vec4(fogColor, 1.0);
+	}
 `
